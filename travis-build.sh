082604d38e346b60ffff10657ad3c9fdc37a0cd1
jaQuan <chinese.jk@gmail.com>
2018-08-15 18:17:25 +0800

    push all build image

--- travis-build.sh
+++ travis-build.sh
@@ -10,12 +10,10 @@ echo '##### Print environment'
 env | sort
 
 #### Build the Docker Images
-if [ -n "${PHP_VERSION}" ]; then
-    cp env-build .env
-    cat .env
-    docker-compose build ${BUILD_SERVICE}
-    docker images
-    docker tag laradock_${BUILD_SERVICE}:latest ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}:auto-build
-    docker push ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}
-fi
 
+cp env-build .env
+cat .env
+docker-compose build ${BUILD_SERVICE}
+docker images
+docker tag laradock_${BUILD_SERVICE}:latest ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}:auto-build
+docker push ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}
