ac37de426bb55e8b84962a602ff3166dfcfd2f42
jaQuan <chinese.jk@gmail.com>
2019-03-28 13:39:01 +0800

    add build version

--- travis-build.sh
+++ travis-build.sh
@@ -11,10 +11,26 @@ env | sort
 
 #### Build the Docker Images
 
+BUILD_VERSION=latest
+
 cp env-build .env
+
+if [ -n "${PHP_VERSION}" ]; then
+    sed -i -- "s/PHP_VERSION=.*/PHP_VERSION=${PHP_VERSION}/g" .env
+    BUILD_VERSION=${PHP_VERSION}
+fi
+
+if [ -n "${MYSQL_VERSION}" ]; then
+    sed -i -- "s/MYSQL_VERSION=.*/MYSQL_VERSION=${MYSQL_VERSION}/g" .env
+    BUILD_VERSION=${MYSQL_VERSION}
+fi
+
+echo  build version is ${BUILD_VERSION}
+
 cat .env
+
 cp docker-compose.build.yml docker-compose.yml
 docker-compose build ${BUILD_SERVICE}
 docker images
-docker tag laradock_${BUILD_SERVICE}:latest ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}:auto-build
+docker tag laradock_${BUILD_SERVICE}:latest ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}:${BUILD_VERSION}
 docker push ${DOCKER_USERNAME}/laradock-${BUILD_SERVICE}
